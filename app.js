40d52d2af1d54ade4a36da5759b24ca8e27a6827
Dauzy <daniel_6287@hotmail.com>
2017-05-16 16:06:38 -0500

    Vistas de login y product

--- app.js
+++ app.js
@@ -4,12 +4,27 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const hbs = require('express-handlebars')
 const app = express()
 const api = require('./routes')
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-//la ruta api usara el modulo api 
+
+app.set('view engine', 'hbs')
+
+app.engine('.hbs', hbs({
+  defaultLayout: __dirname + '/views/layouts/default.hbs',
+  extname: '.hbs'
+}))
+
+//la ruta api usara el modulo api
 app.use('/api',api)
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+app.get('/product', (req, res) => {
+  res.render('product')
+})
 
 module.exports = app
--- controllers/user.js
+++ controllers/user.js
@@ -7,7 +7,8 @@ const service = require('../services');
 function signUp(req, res){
   const user = new User({
     email: req.body.email,
-    displayName: req.body.displayName
+    displayName: req.body.displayName,
+    password: req.body.password
   })
 
   user.save((err) => {
@@ -19,12 +20,15 @@ function signUp(req, res){
 }
 
 function signIn(req, res){
-  user.find({email: req.body.email}, (err,user) => {
+  User.find({email: req.body.email}, (err,user) => {
     if(err) res.status(500).send({message: `Error: ${err}`})
     if(!user) return res.status(404).send({message: `Could not found user :(`})
 
     req.user = user
-    req.status(200).send({message: 'You are loggin! :)', token: service.createToken(user)})
+    res.status(200).send({
+      message: 'You are loggin! :)',
+      token: service.createToken(user)
+    })
 
   })
 }
